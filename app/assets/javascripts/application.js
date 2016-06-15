7211963edf8295e0280927d620e965694b19741e
niceprince <nice.prince.ks@gmail.com>
2016-06-15 20:35:58 +0530

    some file added and changed for ckeditors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require ckeditor/override
 //= require ckeditor/init
 //= require_tree .
--- app/models/ckeditor/application.rb
+++ app/models/ckeditor/application.rb
@@ -1,1 +1,3 @@
 config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w(ckeditor/*)
